c0cf3c8172793a972949526e6cf8f2630892b10e
AetherFabian <fabian_galarza@hotmail.com>
2022-03-22 15:49:53 -0700

    characters details adaptaded api url

--- src/screens/CharacterDetails.js
+++ src/screens/CharacterDetails.js
@@ -2,38 +2,25 @@ import { StyleSheet, View, Text, Image } from "react-native"
 import React from "react"
 
 export default function CharacterDetails(props){
-    const { character } = props.route.params;
+    const { champion } = props;
+    const id = champion["id"];
+    const imageAPI = 'http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/' + id + '.png';
+
     return(
         <View style={styles.container}>
             <View style={styles.content}>
-                <Image style={styles.image} source={{uri: character.image}}/>
+                <Image style={styles.image} source={{uri: imageAPI}}/>
                 <View style={styles.property}>
                     <Text style={styles.property_name}>Name: </Text>
-                    <Text style={styles.property_value}>{character.name}</Text>
+                    <Text style={styles.property_value}>{champion["id"]}</Text>
                 </View>
                 <View style={styles.property}>
-                    <Text style={styles.property_name}>Status: </Text>
-                    <Text style={styles.property_value}>{character.status}</Text>
+                    <Text style={styles.property_name}>Title: </Text>
+                    <Text style={styles.property_value}>{champion["title"]}</Text>
                 </View>
                 <View style={styles.property}>
-                    <Text style={styles.property_name}>Species: </Text>
-                    <Text style={styles.property_value}>{character.species}</Text>
-                </View>
-                <View style={styles.property}>
-                    <Text style={styles.property_name}>Type: </Text>
-                    <Text style={styles.property_value}>{character.type || "NA"}</Text>
-                </View>
-                <View style={styles.property}>
-                    <Text style={styles.property_name}>Gender: </Text>
-                    <Text style={styles.property_value}>{character.gender}</Text>
-                </View>
-                <View style={styles.property}>
-                    <Text style={styles.property_name}>Origin: </Text>
-                    <Text style={styles.property_value}>{character.origin.name}</Text>
-                </View>
-                <View style={styles.property}>
-                    <Text style={styles.property_name}>Location: </Text>
-                    <Text style={styles.property_value}>{character.location.name}</Text>
+                    <Text style={styles.property_name}>Lore: </Text>
+                    <Text style={styles.property_value}>{champion["blurb"]}</Text>
                 </View>
             </View>
         </View>
